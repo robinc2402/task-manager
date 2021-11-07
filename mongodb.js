039eb58d1f3281b596abe945b9a446d6207ed8a5
Robin Chalia <robinc@one.com>
2021-11-07 14:03:21 +0530

    MongoDB: Practiced InsertMany docs with a result cb.

--- mongodb.js
+++ mongodb.js
@@ -11,16 +11,57 @@ MongoClient.connect(connectionURL, { useNewURLParser: true }, (err, client) => {
     return console.log("Unable to connect DB!");
   }
   const db = client.db(databaseName);
-  db.collection("users").insertOne(
-    {
-      name: "ADavid",
-      age: 22,
-    },
-    (error, result) => {
-      if (error) {
-        return console.log("Unable to insert user");
+
+  // insert a single document and know its result with a callback
+  // db.collection("users").insertOne(
+  //   {
+  //     name: "ADavid",
+  //     age: 22,
+  //   },
+  //   (error, result) => {
+  //     if (error) {
+  //       return console.log("Unable to insert user");
+  //     }
+  //     console.log(result);
+  //   }
+  // );
+
+  // insert multiple docs
+  // db.collection("users").insertMany(
+  //   [
+  //     { name: "DeepakR", age: 33 },
+  //     { name: "AnkitT", age: 33 },
+  //     { name: "AkshatT", age: 33 },
+  //   ],
+  //   (error, result) => {
+  //     if (error) {
+  //       return console.log("Unabled to insert docs");
+  //     }
+  //     console.log(result);
+  //   }
+  // );
+
+  // challenge
+  db.collection("tasks").insertMany(
+    [
+      {
+        desc: "Prepare migration endpoints",
+        complete: false,
+      },
+      {
+        desc: "Finalise PHP PPT",
+        complete: false,
+      },
+      {
+        desc: "Migration architecture diagram",
+        complete: true,
+      },
+    ],
+    (err, res) => {
+      if (err) {
+        return console.log("Unable to insert tasks");
       }
-      console.log(result);
+      console.log(res);
     }
   );
 });
