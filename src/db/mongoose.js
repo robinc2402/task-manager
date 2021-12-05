a8a38978be17eb0e3c1ee5923bd4aa2f2405c861
Robin Chalia <robinc@one.com>
2021-12-05 17:21:52 +0530

    MONGOOSE -- Learnt Data models basics

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,21 +5,36 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   // useCreateIndex: true,
 });
 
-const User = mongoose.model("User", {
-  name: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
+// const User = mongoose.model("User", {
+//   name: {
+//     type: String,
+//   },
+//   age: {
+//     type: Number,
+//   },
+// });
+//
+// const me = new User({ name: "Robin", age: 32 });
+//
+// me.save()
+//   .then((res) => {
+//     console.log("Success!", res);
+//   })
+//   .catch((err) => {
+//     console.log("Error! ", err);
+//   });
+
+const Task = mongoose.model("Task", {
+  desc: String,
+  completed: Boolean,
 });
 
-const me = new User({ name: "Robin", age: 32 });
-
-me.save()
+const item = new Task({ desc: "This is task description", completed: false });
+item
+  .save()
   .then((res) => {
-    console.log("Success!", res);
+    console.log("Success", res);
   })
   .catch((err) => {
-    console.log("Error! ", err);
+    console.log("Error", err);
   });
