14d1e3857f1811281d865d0a9ca31e280e1f9410
Robin Chalia <robinc@one.com>
2021-12-06 10:43:12 +0530

    MONGOOSE -- More about Data models + Validations

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,40 +1,58 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
   // useCreateIndex: true,
 });
 
-// const User = mongoose.model("User", {
-//   name: {
-//     type: String,
-//   },
-//   age: {
-//     type: Number,
-//   },
-// });
-//
-// const me = new User({ name: "Robin", age: 32 });
-//
-// me.save()
-//   .then((res) => {
-//     console.log("Success!", res);
-//   })
-//   .catch((err) => {
-//     console.log("Error! ", err);
-//   });
-
-const Task = mongoose.model("Task", {
-  desc: String,
-  completed: Boolean,
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Invalid email");
+      }
+    },
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) throw new Error("Age must be a positive number");
+    },
+  },
 });
 
-const item = new Task({ desc: "This is task description", completed: false });
-item
-  .save()
+const me = new User({ name: "  Robin     ", email: "RoBinC@OnE.dK" });
+
+me.save()
   .then((res) => {
-    console.log("Success", res);
+    console.log("Success!", res);
   })
   .catch((err) => {
-    console.log("Error", err);
+    console.log("Error! ", err);
   });
+
+// const Task = mongoose.model("Task", {
+//   desc: String,
+//   completed: Boolean,
+// });
+//
+// const item = new Task({ desc: "This is task description", completed: false });
+// item
+//   .save()
+//   .then((res) => {
+//     console.log("Success", res);
+//   })
+//   .catch((err) => {
+//     console.log("Error", err);
+//   });
