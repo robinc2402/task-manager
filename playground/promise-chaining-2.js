9223f635767ff86f2bd255644b5a324f7385383c
Robin Chalia <robinc@one.com>
2021-12-23 19:40:42 +0530

    Async Await - 3 (tested with actual DB)

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -8,14 +8,32 @@ const Task = require("../src/models/task");
 // 4. Get and print the total number of incomplete tasks
 // 5. Test your work.
 
-Task.findByIdAndDelete("61c4593c0a7e8bd28b81649f")
+// Task.findByIdAndDelete("61c4593c0a7e8bd28b81649f")
+//   .then((res) => {
+//     console.log("Deleted 61c4593c0a7e8bd28b81649f", res);
+//     return Task.countDocuments({ completed: false });
+//   })
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((e) => {
+//     console.log(e);
+//   });
+
+const taskDeleteAndCount = async (id) => {
+  const task = await Task.findByIdAndDelete(id);
+  // if (!task) {
+  //   return Promise.reject("Could not find/delete task with given iD", id);
+  // }
+  const count = await Task.countDocuments({ completed: false });
+  return count;
+};
+
+// 61ae0a311afc0e8b6e343a4a
+taskDeleteAndCount("61ae0a311afc0e8b6e343a4a")
   .then((res) => {
-    console.log("Deleted 61c4593c0a7e8bd28b81649f", res);
-    return Task.countDocuments({ completed: false });
-  })
-  .then((res) => {
-    console.log(res);
+    console.log("Count of incomplete tasks ", res);
   })
   .catch((e) => {
-    console.log(e);
+    console.log("e", e);
   });
--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -6,13 +6,27 @@ const User = require("../src/models/user");
 // task: find a user with given ID then update his/her age to 21 and count many users with same age are there.
 // We'll first use a Promise to find and Update the age then will chain another Promise to count same age users.
 
-User.findByIdAndUpdate("61c4679c25abec3e09201a6b", { age: 21 })
-  .then((user) => {
-    console.log(user);
-    return User.countDocuments({ age: 21 });
-  })
-  .then((res) => {
-    console.log(res);
+// User.findByIdAndUpdate("61c4679c25abec3e09201a6b", { age: 21 })
+//   .then((user) => {
+//     console.log(user);
+//     return User.countDocuments({ age: 21 });
+//   })
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((e) => {
+//     console.log(e);
+//   });
+
+const updateAgeAndCount = async (id, age) => {
+  const user = await User.findByIdAndUpdate(id, { age });
+  const count = await User.countDocuments({ age });
+  return count;
+};
+
+updateAgeAndCount("61c4679c25abec3e09201a6b", 21)
+  .then((count) => {
+    console.log(count);
   })
   .catch((e) => {
     console.log(e);
