634d570553d1df97607f2297f64432508ddc4a54
Robin Chalia <robinc@one.com>
2021-12-23 17:17:40 +0530

    cleanup

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,29 +4,3 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
   // useCreateIndex: true,
 });
-
-//
-// const me = new User({
-//   name: "  Robin     ",
-//   email: "RoBinC@OnE.dK",
-//   password: "password1",
-// });
-//
-// me.save()
-//   .then((res) => {
-//     console.log("Success!", res);
-//   })
-//   .catch((err) => {
-//     console.log("Error! ", err);
-//   });
-
-//
-// const item = new Task({ desc: "        This is task description     " });
-// item
-//   .save()
-//   .then((res) => {
-//     console.log("Success", res);
-//   })
-//   .catch((err) => {
-//     console.log("Error", err);
-//   });
