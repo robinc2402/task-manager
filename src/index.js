34a9b679aac285a38ee18dbafa57502e6997b55d
Robin Chalia <robinc@one.com>
2021-12-27 19:52:42 +0530

    Resource update practices

--- src/index.js
+++ src/index.js
@@ -78,6 +78,31 @@ app.get("/tasks/:id", async (req, res) => {
   }
 });
 
+app.patch("/users/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdate = ["name", "email", "password", "age"];
+  const isValidUpdate = updates.every((update) => {
+    return allowedUpdate.includes(update);
+  });
+
+  if (!isValidUpdate) {
+    return res.status(400).send({ error: "Invalid update!" });
+  }
+
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
 app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
