ee7dad58231ca921bbc9a10f557a580be7236678
Robin Chalia <robinc@one.com>
2021-12-28 15:32:20 +0530

    Resource DELETE practices

--- src/index.js
+++ src/index.js
@@ -129,6 +129,30 @@ app.patch("/tasks/:id", async (req, res) => {
   }
 });
 
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+app.delete("/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
 app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
